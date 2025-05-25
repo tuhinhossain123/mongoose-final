cd7cfe81bc71a79b2b183d8b997c1afa65a3ad97
tuhinhossain123 <csetuhin55@gmail.com>
2025-05-25 09:14:18 +0600

    Handle department validation when creating & updating document

--- src/app/modules/academic-department/academic-department-model.ts
+++ src/app/modules/academic-department/academic-department-model.ts
@@ -18,6 +18,27 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// ekoi name a jno department create na hoy tar jonno validation
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExists) {
+    throw new Error('This department is allready exists!!');
+  }
+  next();
+});
+
+// deleter kora id diye data update korle update hye jay eta na hoyar validation
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error('This department does not exists!!');
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'academicDepartment',
   academicDepartmentSchema,
