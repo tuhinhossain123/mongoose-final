03bff006f40ab7014254f8a0cd2693744069199e
tuhinhossain123 <csetuhin55@gmail.com>
2025-05-31 11:03:42 +0600

    Intro to JWT , create an accessToken

--- src/app/modules/auth/auth-service.ts
+++ src/app/modules/auth/auth-service.ts
@@ -1,7 +1,9 @@
+import config from '../../config';
 import AppError from '../../errors/appError';
 import { User } from '../user/user-model';
 import { TLoginUser } from './auth-interface';
 import httpStatus from 'http-status';
+import jwt from 'jsonwebtoken';
 
 const loginUser = async (payload: TLoginUser) => {
   const user = await User.isUserExistByCustomId(payload.id);
@@ -11,20 +13,36 @@ const loginUser = async (payload: TLoginUser) => {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!!');
   }
   // checking if the user is already deleted
-  //   const isDeleted = isUserExists?.isDeleted;
-  //   if (!isDeleted) {
-  //     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!!');
-  //   }
+//   const isDeleted = user?.isDeleted;
+//   if (!isDeleted) {
+//     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!!');
+//   }
+
   // checking if the user is blocked
-  //   const userStatus = isUserExists?.status;
-  //   if (userStatus === 'blocked') {
-  //     throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
-  //   }
+  const userStatus = user?.status;
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+  }
 
   //   checking if the user is correct
   if (!(await User.isUserPasswordmatched(payload?.password, user?.password))) {
     throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
   }
+
+  // create token and sent to the client
+
+  const jwtPayload = {
+    userId: user,
+    role: user?.role,
+  };
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+
+  return {
+    accessToken,
+    needsPasswordChange: user?.needsPasswordChange,
+  };
 };
 
 export const authServices = {
